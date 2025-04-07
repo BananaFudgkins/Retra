39f9d898f0fa1cef25888ffb813cd78f5fbcf1dd
Deja Jackson <jacksodl@lafayette.edu>
2025-04-06 18:23:18 -0700

    Install Carbon components and added a basic file uploader

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+    import type { LayoutData } from './$types';
+
+    import "carbon-components-svelte/css/g100.css";
+
+    let { data, children }: { data: LayoutData, children: Snippet } = $props();
+</script>
+
+{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import {
+        Header,
+        HeaderNav,
+        HeaderNavItem,
+        HeaderNavMenu,
+        SkipToContent,
+        Content,
+        Grid,
+        Row,
+        Column,
+        FileUploader,
+    } from "carbon-components-svelte";
+
+    function fileAdded(event: CustomEvent<ReadonlyArray<File>>) {
+        const files = event.detail;
+        console.log("Files added: ", files);
+
+        for (const file of files) {
+            console.log(file.name, file.size);
+        }
+    }
+</script>
+
+<Header company="Deja Jackson" platformName="Retra">
+
+</Header>
+
+<Content>
+    <Grid>
+        <Row>
+            <Column>
+                <h1>Welcome</h1>
+            </Column>
+        </Row>
+        <Row>
+            <Column>
+                <FileUploader
+                    multiple
+                    labelTitle="Upload files"
+                    buttonLabel="Add files"
+                    labelDescription="Upload your Spotify extended listening history. Only JSON files are accepted."
+                    accept={[".json"]}
+                    status="complete"
+                    on:add={fileAdded}
+                />
+            </Column>
+        </Row>
+    </Grid>
+</Content>
