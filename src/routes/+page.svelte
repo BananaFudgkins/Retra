b57594f91401b0c6984acbb25165807abb638daf
Deja Jackson <jacksodl@lafayette.edu>
2025-04-20 13:27:57 -0700

    Dates will be extracted out of timestamps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,28 +12,50 @@
         FileUploader,
     } from "carbon-components-svelte";
 
+    type SpotifyListen = {
+        ts: string;
+        ms_played: number;
+        track_name: string;
+        artist_name: string;
+        album_name: string;
+        shuffle: boolean;
+        skipped: boolean;
+    }
+
     let parsedData: string[] = [];
+
+    function isSpotifyListenArray(data: any): data is SpotifyListen[] {
+        return Array.isArray(data) && data.every( item =>
+            typeof item.ts === 'string' &&
+            typeof item.ms_played === 'number'
+        );
+    }
 
     async function onAdd(event: CustomEvent<ReadonlyArray<File>>) {
         const files = event.detail;
+        const allListens: SpotifyListen[] = [];
 
         for (const file of files) {
             if (file.type === "application/json") {
-                let json = await readJson(file);
-                parsedData.push(json as string);
+                const text = await file.text();
+                let json = JSON.parse(text);
+                
+                if (isSpotifyListenArray(json)) {
+                    allListens.push(...json);
+                }
             }
         }
 
-        console.log("Parsed", parsedData.length, "files.");
-    }
+        const grouped: Record<string, SpotifyListen[]> = {};
 
-    function readJson(file: File) {
-        const reader = new FileReader();
-        return new Promise((resolve, reject) => {
-            reader.onload = () => resolve(JSON.parse(reader.result as string));
-            reader.onerror = reject;
-            reader.readAsText(file);
+        allListens.forEach((listen) => {
+            const date = new Date(listen.ts).toISOString().split('T')[0];
+            if (!grouped[date]) grouped[date] = [];
+            grouped[date].push(listen);
         });
+
+        console.log("Parsed", parsedData.length, "files.");
+        console.log(parsedData);
     }
 </script>
 
